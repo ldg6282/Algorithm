6799e70404292ae12e66d4d97a136cc74e3d469a
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2025-05-28 11:36:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 68.14 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const answer = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === answer[answer.length - 1]) {
+            answer.pop();
+        } else {
+            answer.push(s[i]);
+        }
+    }
+    
+    return answer.length === 0 ? 1 : 0;
+}

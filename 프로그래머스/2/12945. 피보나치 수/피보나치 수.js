0168db53b5494a11d0fb6f799bf00bf76ad94533
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2025-05-28 11:20:31 +0900

    [level 2] Title: 피보나치 수, Time: 36.42 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const arr = [0, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        arr.push(arr[i - 1] % 1234567 + arr[i - 2] % 1234567);
+    }
+    
+    return arr[arr.length - 1] % 1234567;
+}

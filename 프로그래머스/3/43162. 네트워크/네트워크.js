665c291ded17d24bd893e03200bf2c905f1c104f
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2025-06-16 16:00:49 +0900

    [level 3] Title: 네트워크, Time: 0.66 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+    const visited = Array(n).fill(false);
+    let answer = 0;
+    
+    function dfs(i) {
+        visited[i] = true;
+
+        for (let j = 0; j < n; j++) {
+            if (computers[i][j] === 1 && !visited[j]) {
+                dfs(j);
+            }
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            answer++;
+        }
+    }
+    
+    return answer;
+}

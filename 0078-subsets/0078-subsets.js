353d2d8a16a74cec4b62d30c57ceee7964f43f57
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2025-07-10 12:03:15 +0900

    Time: 0 ms (100%), Space: 56 MB (19.54%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const result = [];
+    
+    function dfs(index, path) {
+        result.push([...path]);
+        for (let i = index; i < nums.length; i++) {
+            path.push(nums[i]);
+            dfs(i + 1, path);
+            path.pop();
+        }
+    }
+
+    dfs(0, []);
+    return result;
+};
